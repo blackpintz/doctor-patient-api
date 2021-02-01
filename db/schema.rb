c44fbc8a028ff20e377c5a48e2500762525dfd08
Ubuntu <ubuntu@ip-172-31-29-80.us-east-2.compute.internal>
2021-02-01 19:18:17 +0000

    remove trackable from Devise model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_01_101857) do
+ActiveRecord::Schema.define(version: 2021_02_01_123036) do
+
+  create_table "physicians", force: :cascade do |t|
+    t.string "category"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
