59ab099cab336a435aeab3422917e799dee9a094
Ubuntu <ubuntu@ip-172-31-29-80.us-east-2.compute.internal>
2021-02-01 11:55:48 +0000

    configured cors.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://frozen-harbor-46293.herokuapp.com/'
+    origins 'http://localhost:3000/'
 
     resource '*',
       headers: :any,
