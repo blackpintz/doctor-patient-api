20ee604e824cca2f00000ffe27a917a367ea8e66
Ubuntu <ubuntu@ip-172-31-29-80.us-east-2.compute.internal>
2021-02-01 10:07:38 +0000

    set up github actions.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,4 +49,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.hosts << "156ab47aea2d4d3c98c3e44f811da4a4.vfs.cloud9.us-east-2.amazonaws.com"
 end
