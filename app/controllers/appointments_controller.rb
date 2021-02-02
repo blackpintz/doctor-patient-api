3639a7b49b7f132cde215785f18233ea6a0a551b
Ubuntu <ubuntu@ip-172-31-29-80.us-east-2.compute.internal>
2021-02-02 14:12:28 +0000

    add authentication for appointments.

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,7 +1,7 @@
 class AppointmentsController < ApplicationController
     before_action :authenticate_user!
     def index
-        appointments = current_user
+        appointments = current_user.appointments
         render json: appointments
     end
 end
