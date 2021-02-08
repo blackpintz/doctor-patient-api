506be47460391dbd325086351e68b0ef0530c540
Ubuntu <ubuntu@ip-172-31-29-80.us-east-2.compute.internal>
2021-02-08 13:08:37 +0000

    fix linter errors.

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Appointment, type: :model do
-    describe 'validations' do
-         it { should validate_presence_of(:city) }
-         it { should validate_length_of(:city).is_at_least(3) }
-    end
-    describe 'associations' do
-        it { should belong_to(:user) }
-        it { should belong_to(:physician) }
-    end
+  describe 'validations' do
+    it { should validate_presence_of(:city) }
+    it { should validate_length_of(:city).is_at_least(3) }
+  end
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:physician) }
+  end
 end
