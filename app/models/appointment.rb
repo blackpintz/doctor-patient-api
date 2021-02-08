f8d5d0b7bbcfe1c7b8d30e3783e542a34f017a21
Ubuntu <ubuntu@ip-172-31-29-80.us-east-2.compute.internal>
2021-02-08 12:58:36 +0000

    add spec tests for my models.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,5 +1,5 @@
 class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :physician
-  validates :city, presence: true, length: { minimum: 2 }
+  validates :city, presence: true, length: { minimum: 3 }
 end
